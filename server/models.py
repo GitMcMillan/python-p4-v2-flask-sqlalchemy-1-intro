6a4099baa110850d854737752e0284c92fad16e7
alexandermcmillan <mcmillancontact@protonmail.com>
2024-10-04 17:50:41 -0700

    finished code-along

--- server/models.py
+++ server/models.py
@@ -16,3 +16,16 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    #id = primary key
+    id = db.Column(db.Integer, primary_key=True)
+    #username is unique str of 80 length. Null values NOT allowed. Index is set on the column to speed up queries when searched by this column
+    username = db.Column(db.String(80), unique=True,
+                         nullable=False, index=True)
+    # unique string of length 120. Null values allowed
+    email = db.Column(db.String(120), unique=True)
+    #boolean value. Defaults to False if a value not given
+    verified = db.Column(db.Boolean, default=False)
